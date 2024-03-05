7e77be5ccbc7bbf17bd9b591e2886b9cacc79939
bitdruid <bitdruid@outlook.com>
2024-03-05 22:58:22 +0100

    use hexadecimal encoding

--- encode.py
+++ encode.py
@@ -77,39 +77,42 @@ B64_RGB_DICT = {
     " ": (255, 255, 255)
 }
 
-def encode(filepath):
+def encode_hex(filepath):
     try:
-        filename = os.path.basename(filepath)
-        filename = base64.b64encode(filename.encode("utf-8"))
+        filename = os.path.basename(filepath) + "\n\r"
+        filename = filename.encode("utf-8").hex()
         with open(filepath, "rb") as file:
             file_content = file.read()
-            file_content_compressed = zlib.compress(file_content, 9)
-            encoded_file = filename + b"\n\r" + base64.b64encode(file_content_compressed)
+            #file_content_compressed = zlib.compress(file_content, 9)
+            encoded_file = filename + file_content.hex()
     except FileNotFoundError:
         print(f"File '{filepath}' not found.")
-    
+
     return encoded_file
 
-def create_image_content(encoded_string):
-    content_length = len(encoded_string)
-    content_length_root = math.sqrt(content_length)
-    if content_length_root.is_integer():
-        image_size = content_length_root
-        content_addition = 0
-    else:
-        image_size = math.ceil(content_length_root)
-        content_addition = image_size ** 2 - content_length
-    pixel_matrix = []
-    for char in encoded_string:
-        pixel_matrix.append(B64_RGB_DICT[chr(char)])
-    for _ in range(content_addition):
-        pixel_matrix.append(B64_RGB_DICT[" "])
-    return pixel_matrix
+def create_image_content_hex(encoded_content):
+    # end recognized by: \n\r and one 0x00 byte
+    # \n = 0x0a, \r = 0x0d
+    # append end delimiter
+    encoded_content += "0a0d"
+    rest = (6 - len(encoded_content) % 6) // 2
+    # append 0x00 bytes to fill up the last 6 byte block
+    if rest != 6:
+        encoded_content += "00" * rest
+    # split encoded content into 6 byte blocks
+    hex_pixel_matrix = [encoded_content[i:i+6] for i in range(0, len(encoded_content), 6)]
+    # append 0x00 bytes until the length of the hex pixel matrix is a perfect square
+    hex_pixel_matrix_root = math.sqrt(len(hex_pixel_matrix))
+    if not hex_pixel_matrix_root.is_integer():
+        hex_pixel_matrix += ["000000"] * (int(hex_pixel_matrix_root + 1) ** 2 - len(hex_pixel_matrix))
+    print(hex_pixel_matrix)
+    return hex_pixel_matrix
 
-def create_image(image_content):
-    image_size = math.sqrt(len(image_content))
+def create_image_hex(hex_pixel_matrix):
+    rgb_pixel_matrix = [tuple(int(hex_pixel[i:i+2], 16) for i in (0, 2, 4)) for hex_pixel in hex_pixel_matrix]
+    image_size = math.sqrt(len(rgb_pixel_matrix))
     image = PIL.Image.new("RGB", (int(image_size), int(image_size)))
-    image.putdata(image_content)
+    image.putdata(rgb_pixel_matrix)
     image.save("encoded.png")
 
 def main():
@@ -118,10 +121,13 @@ def main():
         sys.exit(1)
     else:
         filepath = sys.argv[1]
-        encoded_file = encode(filepath)
-        image_content = create_image_content(encoded_file)
-        create_image(image_content)
-        print("File encoded.")
+        encoded_content = encode_hex(filepath)
+        image_content = create_image_content_hex(encoded_content)
+        create_image_hex(image_content)
+        # encoded_file = encode(filepath)
+        # image_content = create_image_content(encoded_file)
+        # create_image(image_content)
+        # print("File encoded.")
 
 if __name__ == "__main__":
     main()
